764472436ac4a63789b67066b921a0cbc133ad22
cargau <57837914+cargau@users.noreply.github.com>
2021-03-17 12:21:26 -0400

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,46 @@
+function actionTout2 () {
+	
+}
+function chec_aouch2 () {
+	
+}
 function actionMechant () {
     if (mechantY == 5) {
-        let myImage = 0
-        mechantX = myImage
-        mechantY = randint(0, 4)
+        mechantY = 0
+        mechantX = randint(0, 4)
     }
-    basic.pause(100)
+    basic.pause(4)
     led.unplot(mechantX, mechantY)
-    basic.pause(4)
+    basic.pause(16)
     mechantY = mechantY + 1
-    basic.pause(4)
+    basic.pause(100)
     led.plot(mechantX, mechantY)
+}
+function chec_aouch4 () {
+	
 }
 function actionTout () {
     acitonBala()
-    chec_aouch()
     actionMechant()
+    chec_aouch()
+}
+function vieMechant5 () {
+	
+}
+function actionTout3 () {
+	
+}
+function acitonBala4 () {
+	
+}
+function acitonBala3 () {
+	
+}
+function actionMechant5 () {
+	
+}
+function acitonBala5 () {
+	
 }
 input.onButtonPressed(Button.A, function () {
     if (jugadorX > 0) {
@@ -23,6 +49,18 @@ input.onButtonPressed(Button.A, function () {
         led.plot(jugadorX, JUGADORY)
     }
 })
+function vieMechant4 () {
+	
+}
+function actionMechant2 () {
+	
+}
+function actionMechant4 () {
+	
+}
+function actionMechant3 () {
+	
+}
 input.onButtonPressed(Button.AB, function () {
     balaX = jugadorX
     balaY = JUGADORY - 1
@@ -37,14 +75,27 @@ input.onButtonPressed(Button.AB, function () {
     led.plot(balaX, balaY)
     basic.pause(1)
 })
+function vieMechant () {
+    if (mechantX == balaX && mechantY == balaY) {
+        led.unplot(mechantX, mechantY)
+        music.playMelody("- - C5 C5 C5 C5 C5 C5 ", 598)
+    }
+}
 function chec_aouch () {
     if (jugadorX == mechantX && JUGADORY == mechantY) {
-        music.playMelody("C - - - - - - - ", 500)
+        music.playMelody("C - - - - - - - ", 598)
     }
+}
+function chec_aouch5 () {
+	
+}
+function vieMechant2 () {
+	
 }
 function acitonBala () {
     led.unplot(balaX, balaY)
     balaY = balaY - 1
+    basic.pause(1)
     led.plot(balaX, balaY)
 }
 input.onButtonPressed(Button.B, function () {
@@ -54,6 +105,21 @@ input.onButtonPressed(Button.B, function () {
         led.plot(jugadorX, JUGADORY)
     }
 })
+function vieMechant3 () {
+	
+}
+function actionTout5 () {
+	
+}
+function chec_aouch3 () {
+	
+}
+function acitonBala2 () {
+	
+}
+function actionTout4 () {
+	
+}
 let mechantY = 0
 let mechantX = 0
 let balaY = 0
