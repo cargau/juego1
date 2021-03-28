46ceb6437d8c23324d2cb5eeed6a1d0749e792c9
cargau <57837914+cargau@users.noreply.github.com>
2021-03-28 10:14:41 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,18 @@
 function actionMechant () {
-    vieMechant()
+    ouchMechant()
     if (mechantY == 5) {
         spawnMechant()
     }
-    basic.pause(4)
     led.unplot(mechantX, mechantY)
-    basic.pause(16)
     mechantY = mechantY + 1
-    basic.pause(100)
     led.plot(mechantX, mechantY)
 }
 function actionTout () {
     actionMechant()
-    chec_aouch()
-    acitonBala5()
 }
 function spawnMechant () {
-    mechantY = 0
-    mechantX = randint(2, 2)
-}
-function acitonBala5 () {
-    led.unplot(balaX, balaY)
-    balaY = balaY - 1
-    basic.pause(1)
-    led.plot(balaX, balaY)
+    mechantY = -1
+    mechantX = randint(0, 4)
 }
 input.onButtonPressed(Button.A, function () {
     if (jugadorX > 0) {
@@ -32,6 +21,13 @@ input.onButtonPressed(Button.A, function () {
         led.plot(jugadorX, JUGADORY)
     }
 })
+function ouchMechant () {
+    if (mechantX == balaX && mechantY == balaY) {
+        led.unplot(mechantX, mechantY)
+        music.playMelody("- - - - - - - C5 ", 106)
+        spawnMechant()
+    }
+}
 input.onButtonPressed(Button.AB, function () {
     balaX = jugadorX
     balaY = JUGADORY - 1
@@ -46,17 +42,17 @@ input.onButtonPressed(Button.AB, function () {
     led.plot(balaX, balaY)
     basic.pause(1)
 })
-function vieMechant () {
-    if (mechantX == balaX && mechantY == balaY) {
-        led.unplot(mechantX, mechantY)
-        music.playMelody("C - - - - - - - ", 106)
-        spawnMechant()
-    }
-}
 function chec_aouch () {
     if (jugadorX == mechantX && JUGADORY == mechantY) {
         music.playMelody("C - - - - - - - ", 598)
+        led.plot(jugadorX, JUGADORY)
     }
+}
+function actionBala () {
+    led.unplot(balaX, balaY)
+    balaY = balaY - 1
+    basic.pause(1)
+    led.plot(balaX, balaY)
 }
 input.onButtonPressed(Button.B, function () {
     if (jugadorX < 4) {
@@ -65,10 +61,10 @@ input.onButtonPressed(Button.B, function () {
         led.plot(jugadorX, JUGADORY)
     }
 })
-let mechantY = 0
-let mechantX = 0
 let balaY = 0
 let balaX = 0
+let mechantY = 0
+let mechantX = 0
 let JUGADORY = 0
 let jugadorX = 0
 let vies = 1
@@ -76,11 +72,9 @@ let poinVies = 16
 jugadorX = 2
 JUGADORY = 4
 led.plot(jugadorX, JUGADORY)
-balaX = 2
-balaY = 4
 mechantX = 2
 mechantY = 0
-led.plot(mechantX, 0)
+led.plot(mechantX, mechantY)
 basic.forever(function () {
     actionTout()
 })
